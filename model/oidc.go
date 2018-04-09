513d885d254260706c3ba8425f85fb6879fd5d73
Sarah Collison <sarah@telmate.com>
2018-04-09 09:08:00 -0700

    Add .Experimental.Oidc.UsernamePrefix and .Experimental.Oidc.GroupsPrefix to OIDC config in cluster.yaml

--- model/oidc.go
+++ model/oidc.go
@@ -1,9 +1,11 @@
 package model
 
 type Oidc struct {
-	Enabled       bool   `yaml:"enabled"`
-	IssuerUrl     string `yaml:"issuerUrl"`
-	ClientId      string `yaml:"clientId"`
-	UsernameClaim string `yaml:"usernameClaim"`
-	GroupsClaim   string `yaml:"groupsClaim,omitempty"`
+	Enabled         bool   `yaml:"enabled"`
+	IssuerUrl       string `yaml:"issuerUrl"`
+	ClientId        string `yaml:"clientId"`
+	UsernameClaim   string `yaml:"usernameClaim"`
+	GroupsClaim     string `yaml:"groupsClaim,omitempty"`
+	UsernamePrefix  string `yaml:"usernamePrefix,omitempty"`
+	GroupsPrefix    string `yaml:"groupsPrefix,omitempty"`
 }
